ad5f896ac6a1ccbe7b39dec6e5b0f650808956df
CpE Laboratory <CpE Laboratory@BE216-PC06>
2026-04-15 14:10:52 +0800

    Week one done

--- Task5_Descending_OOP/Form1.Designer.cs
+++ Task5_Descending_OOP/Form1.Designer.cs
@@ -64,7 +64,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(711, 450);
             Controls.Add(label1);
             Controls.Add(btnSort);
             Controls.Add(txtInput);
